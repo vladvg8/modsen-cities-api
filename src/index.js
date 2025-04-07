27acc457e8fe16c48442ff69f57a7073c48cc9b0
vladvg8 <vlad.vg8@gmail.com>
2025-04-07 18:18:56 +0300

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const SERVER_HOST = process.env.SERVER_HOST || 'localhost';
 const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   logger.info(`Receive request from ${req.ip}`);
